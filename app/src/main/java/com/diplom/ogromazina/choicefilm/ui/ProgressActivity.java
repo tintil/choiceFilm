61872d8d3a8bc0533c7e1e0dbe8bab10ca28280a
Olya Gromazina <OGromazina@ad.harman.com>
2018-05-05 13:46:56 +0300

    Add method of factorization

---
+++ app/src/main/java/com/diplom/ogromazina/choicefilm/ui/ProgressActivity.java
@@ -0,0 +1,15 @@
+package com.diplom.ogromazina.choicefilm.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.diplom.ogromazina.choicefilm.R;
+
+public class ProgressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress);
+    }
+}
